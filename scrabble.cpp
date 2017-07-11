39d9164b862b0637807bcb7b19472d49feb0dfbc
jethington <jethington4@gmail.com>
2017-07-10 21:19:21 -0400

    Deleted code related to old solution, not used anymore.

--- scrabble.cpp
+++ scrabble.cpp
@@ -8,14 +8,10 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <unordered_set>
 
 int value(std::string word);
 int char_to_index(char c);
-std::tuple<std::string, int> highest_value_brute_force(std::string tiles);
 std::tuple<std::string, int> highest_value(std::string tiles);
-std::vector<std::string> generate_permutations(std::string tiles);
-void generate_permutations_helper(std::string tiles_left, std::string permutation, std::vector<std::string>& results);
 
 struct Node {
 	Node* next[26];
@@ -43,7 +39,6 @@ struct Node {
 
 const int tile_values[26] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10}; // values of A through Z
 
-std::unordered_set<std::string> words_set;
 Node tree;
 
 //std::tuple(std::string, int) Node::best_score(std::string tiles_used, std::string tiles_left) {
@@ -114,53 +109,6 @@ int value(std::string word) {
     return total_value;
 }
 
-// TODO: rename these functions
-std::vector<std::string> generate_permutations(std::string tiles) {
-    std::vector<std::string> results;
-    generate_permutations_helper(tiles, "", results);
-    return results;
-}
-
-void generate_permutations_helper(std::string tiles_left, std::string permutation, std::vector<std::string>& results) {   
-    for (int i = 0; i < tiles_left.length(); i++) {
-        char c = tiles_left[i];
-        std::string tiles_left_copy = tiles_left;
-        std::string permutation_copy = permutation;
-  
-        std::swap(tiles_left_copy[i], tiles_left_copy.back());
-        tiles_left_copy.pop_back();
-        permutation_copy.push_back(c);
-
-        if (words_set.count(permutation_copy) != 0) {
-            results.push_back(permutation_copy); // this permutation is in the list of english words, so add it to the results vector
-        }
-
-        if (tiles_left_copy.length() == 0) {            
-            // done, just return
-        }
-        else {
-            generate_permutations_helper(tiles_left_copy, permutation_copy, results);
-        }
-    }
-}
-
-std::tuple<std::string, int> highest_value_brute_force(std::string tiles) {
-    // brute force approach
-
-    std::vector<std::string> permutations = generate_permutations(tiles);
-
-    int highest = 0;
-    std::string best;
-    for (const std::string& s: permutations) {
-        if (value(s) > highest) {
-            best = s;
-            highest = value(s);
-        }
-    }
-
-    return std::make_tuple(best, highest);
-}
-
 std::tuple<std::string, int> highest_value(std::string tiles) {
     // use tree instead
     std::vector<std::string> results;
@@ -185,23 +133,6 @@ void run_tests(void) {
 
     // test 'value'
     assert(value("daily") == 31);
-    
-    // test 'generate_permutations'
-    // TODO: need to modify testing strategy, this currently fails due to word list
-    /*std::vector<std::string> permutations = generate_permutations("abc");
-    assert(permutations.size() == 6);
-    assert(permutations[0] == "acb");
-    assert(permutations[1] == "abc");
-    assert(permutations[2] == "bac");
-    assert(permutations[3] == "bca");
-    assert(permutations[4] == "cab");
-    assert(permutations[5] == "cba");*/
-
-    // test 'highest_value_brute_force'
-    // Note: this is pretty slow, about 6-10 seconds with -O2
-    //assert(highest_value_brute_force("iogsvooely") == std::make_tuple("oology", 44));
-    //assert(highest_value_brute_force("seevurtfci") == std::make_tuple("service", 52));
-    //assert(highest_value_brute_force("vepredequi") == std::make_tuple("reequip", 78));
 
     // test 'Node::add_string'
     Node t;
@@ -233,13 +164,11 @@ void run_tests(void) {
 }
 
 int main(void) {
-
    
     // read word list into a tree   
     std::ifstream in_file("enable1.txt"); 
     std::string word;
     while (in_file >> word) {
-        words_set.insert(word);
         tree.add_string(word);
     }
 
